2c18fe15e5a94b50883b9a73a9eb78a61ae0464c
David Parry <drjparry@gmail.com>
2016-01-11 20:03:20 +0100

    updated files to test for 3, 5, 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,24 @@
-def fizzbuzz(number)
+class Integer
+
+def fizzbuzz
+  if self % 5 == 0 && self % 3 == 0
+  'fizzbuzz'
+  elsif self % 3 == 0
   'fizz'
+  elsif self % 5 == 0
+  'buzz'
+
+
+end
+
+
+
+end
+
+
+
+
+
+
+
 end
